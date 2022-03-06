930d2f7d7bbf8a81439ec8d8dcc491eb07f90a9f
RyuBotz <76149388+unknowkzr@users.noreply.github.com>
2022-03-06 11:01:05 +0700

    Add files via upload

---
+++ replit.nix
@@ -0,0 +1,12 @@
+{ pkgs }: {
+    deps = [
+        pkgs.nodejs
+        pkgs.nodePackages.typescript
+        pkgs.ffmpeg
+        pkgs.imagemagick
+        pkgs.git
+        pkgs.neofetch
+        pkgs.libwebp
+        pkgs.speedtest-cli
+    ];
+}
